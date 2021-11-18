8aea38e3a8eb9cd6e1f246f170c3f1192d11271e
Sergio Chacon <36614480+sachacon@users.noreply.github.com>
2021-11-18 00:22:12 -0600

    Update and rename unsigned_mult_8x8_tb.v to unsigned_mult_tb.v

--- tb/unsigned_mult_tb.v
+++ tb/unsigned_mult_tb.v
@@ -1,17 +1,18 @@
-module unsigned_mult_8x8_tb;
+module unsigned_mult_tb
+  #(parameter BIT_WIDTH=8);
   
-  reg [7:0]A;
-  reg [7:0]X;
-  wire [15:0]C;
+  reg [BIT_WIDTH-1:0]A;
+  reg [BIT_WIDTH-1:0]X;
+  wire [(2*BIT_WIDTH)-1:0]C;
   
-  unsigned_mult_8x8 uut (.A(A), .X(X), .C(C));
+  unsigned_mult uut (.A(A), .X(X), .C(C));
   
   initial begin
     // A is a constant, X is the input 
-    A = 8'd2; X = 8'd0;
+    A = 8'd3; X = 8'd0;
     #5 
     $display("time=%0t, A=%0d, X=%0d. C=%0d", $time, A, X, C);
-    for(int i = 0; i < 256; i++)
+    for(int i = 0; i < (1 << BIT_WIDTH); i++)
       begin
         X = X + 8'd1;
         #5
