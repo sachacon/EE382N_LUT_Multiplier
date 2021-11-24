388910b07e092205ece2c7612d9aed6d6939841b
Sergio Chacon <36614480+sachacon@users.noreply.github.com>
2021-11-24 17:03:03 -0600

    Update 8bit_lut_mult.v

--- src/8bit_lut_mult.v
+++ src/8bit_lut_mult.v
@@ -3,7 +3,15 @@ module 8bit_lut_mult #(parameter A_const = 2) (input [7:0]X, output [15:0]C);
   // input coding unit 
   // verify input coding unit block
   wire [2:0] input_coding_o;
-  input_coding u_input_coding (.x(X[3:0]), .y(input_coding_o))
+  // input_coding u_input_coding (.x(X[3:0]), .y(input_coding_o))
+  // Xi’ = (Xi XOR Ci) + (Ci-1 XOR Ci)
+  // Ci-1 = 0, (Ci-1 XOR Ci) = (0 XOR Ci) = Ci 
+  // Ci = X[3]
+  assign input_coding_o[0] = (X[0] ^ X[3]) | X[3]; 
+  assign input_coding_o[1] = (X[1] ^ X[3]) | X[3]; 
+  assign input_coding_o[2] = (X[2] ^ X[3]) | X[3]; 
+  assign input_coding_o[3] = (X[3] ^ X[3]) | X[3]; 
+  // input_coding_o[3:0] = (X[3:0] ^ X[3]) | X[3]; 
   
   
   // 8 word direct LUT
