15b13883bc9a57b18cc012d8a9ffdc831b7d887a
Sergio Chacon <36614480+sachacon@users.noreply.github.com>
2021-11-24 16:10:15 -0600

    Update 8bit_lut_mult.v

--- src/8bit_lut_mult.v
+++ src/8bit_lut_mult.v
@@ -1,6 +1,7 @@
 module 8bit_lut_mult #(parameter A_const = 2) (input [7:0]X, output [15:0]C); 
   
   // input coding unit 
+  // verify input coding unit block
   wire [2:0] input_coding_o;
   input_coding u_input_coding (.x(X[3:0]), .y(input_coding_o))
   
@@ -10,14 +11,14 @@ module 8bit_lut_mult #(parameter A_const = 2) (input [7:0]X, output [15:0]C);
   always@(*)
     begin
       case(input_coding_o)
-        3'b000: direct_lut_o = 11'd0 * A_const;
-        3'b001: direct_lut_o = 11'd1 * A_const;
-        3'b010: direct_lut_o = 11'd2 * A_const; 
-        3'b011: direct_lut_o = 11'd3 * A_const;
-        3'b100: direct_lut_o = 11'd4 * A_const;
-        3'b101: direct_lut_o = 11'd5 * A_const; 
-        3'b110: direct_lut_o = 11'd6 * A_const;
-        3'b111: direct_lut_o = 11'd7 * A_const;
+        3'b000: direct_lut_o = 16'd0 * A_const;
+        3'b001: direct_lut_o = 16'd1 * A_const;
+        3'b010: direct_lut_o = 16'd2 * A_const; 
+        3'b011: direct_lut_o = 16'd3 * A_const;
+        3'b100: direct_lut_o = 16'd4 * A_const;
+        3'b101: direct_lut_o = 16'd5 * A_const; 
+        3'b110: direct_lut_o = 16'd6 * A_const;
+        3'b111: direct_lut_o = 16'd7 * A_const;
       endcase 
     end 
   
@@ -31,28 +32,55 @@ module 8bit_lut_mult #(parameter A_const = 2) (input [7:0]X, output [15:0]C);
   increment_circuit u_incr2 (.A(X[7:4]), .incr(X[3]), .S(incr_o[3:0]), .Co(incr_o[4]));
   
   // 9 word oms LUT
+  // same thing as Table IV 
   reg [15:0] oms_lut_o;
   always@(*)
     begin
       case(incr_o)
-        5'b00000: oms_lut_o = 12'd0;
-        5'b00001: oms_lut_o = 12'd1;
-        5'b00001: oms_lut_o = 12'd2;
-        5'b00001: oms_lut_o = 12'd3;
-        5'b00001: oms_lut_o = 12'd4;
-        5'b00001: oms_lut_o = 12'd5;
-        5'b00001: oms_lut_o = 12'd6;
-        5'b00001: oms_lut_o = 12'd7;
-        5'b10000: oms_lut_o = 12'd8;
-        default: oms_lut_o = 12'b0
+        5'b00000: oms_lut_o = 16'd0;
+        5'b00001: oms_lut_o = 16'd1;
+        5'b00011: oms_lut_o = 16'd2;
+        5'b00001: oms_lut_o = 16'd3;
+        5'b00001: oms_lut_o = 16'd4;
+        5'b00001: oms_lut_o = 16'd5;
+        5'b00001: oms_lut_o = 16'd6;
+        5'b00001: oms_lut_o = 16'd7;
+        5'b10000: oms_lut_o = 16'd8;
+        default: oms_lut_o = 16'b0
       endcase
     end 
         
   // barrel shifter 
-  // 12bit_barrel_shifter u_shift ( ); 
+  // add control bits, assume the same s0 and s1 equations as the paper
+  // 12bit_barrel_shifter u_shift ( );
+          
+  wire s0; wire s1;
+  // s0 
+  not u00 (n00, y[2]);
+  or  u01 (n01, y[1], n00);
+  not u02 (n02, n01);
+  or  u03 (n03, y[0], n02);
+  not u04 (s0, n03);
+  
+  // s1
+  or s10  (n10, y[0], y[1]);
+  not s11 (s1, n10);
+  
+  reg [15:0] temp_shift_out;
+  wire [15:0] shift_out assign = temp_shift_out;
+  always@(*)
+    case({s1,s0})
+      2'b00: temp_shift_out = shift_in;
+      2'b01: temp_shift_out = shift_in << 1;
+      2'b10: temp_shift_out = shift_in << 2;
+      2'b11: temp_shift_out = shift_in << 3;
+    endcase 
+ 
   
   // add partial products to get final product 
-  // 16bit_adder u_adder (.A(), .B(), .S(C);
+   C = {4'b0,sign_mod_o} + {temp_shift_out[11:0],4'b0} ; // a[15:0], b[15:0]
+   // C = {8'b0,sign_mod_o} + {temp_shift_out[15:0],4'b0} ; // a[15:0], b[15:0
+   // 16bit_adder u_adder (.A(), .B(), .S(C);
   
 endmodule 
  
