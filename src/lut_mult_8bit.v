b5848acc09ea060e965ad5617823e71c0374c5db
Sergio Chacon <36614480+sachacon@users.noreply.github.com>
2021-11-24 23:24:48 -0600

    fixed some syntax errors

--- src/lut_mult_8bit.v
+++ src/lut_mult_8bit.v
@@ -4,7 +4,7 @@ module lut_mult_8bit #(parameter A_const = 2) (input [7:0]X, output [15:0]C);
   // verify input coding unit block
   wire [2:0] input_coding_o;
   // input_coding u_input_coding (.x(X[3:0]), .y(input_coding_o))
-  // Xi’ = (Xi XOR Ci) + (Ci-1 XOR Ci)
+  // Xi' = (Xi XOR Ci) + (Ci-1 XOR Ci)
   // Ci-1 = 0, (Ci-1 XOR Ci) = (0 XOR Ci) = Ci 
   // Ci = X[3]
   assign input_coding_o[0] = (X[0] ^ X[3]) | X[3]; 
@@ -12,9 +12,9 @@ module lut_mult_8bit #(parameter A_const = 2) (input [7:0]X, output [15:0]C);
   assign input_coding_o[2] = (X[2] ^ X[3]) | X[3]; 
   assign input_coding_o[3] = (X[3] ^ X[3]) | X[3]; 
   // input_coding_o[3:0] = (X[3:0] ^ X[3]) | X[3]; 
+ 
   
-  
-  // 8 word direct LUT
+  // 8 Word Direct LUT
   reg [10:0] direct_lut_o;
   always@(*)
     begin
@@ -30,32 +30,27 @@ module lut_mult_8bit #(parameter A_const = 2) (input [7:0]X, output [15:0]C);
       endcase 
     end 
   
-  // sign modification 
+  // Sign Modification 
   wire [11:0] sign_mod_o;
-  // sign_modification u_sign_mod (.x(direct_lut_o), .incr(X[3]), .y(sign_mod_o));
   wire [10:0] inv_bits;
- 
-  assign inv_bits = ~x;
-  assign sign_mod_o = sign_mod_temp;
-  reg [11:0] sign_mod_temp; 
+  reg [11:0] sign_mod_temp;
+  assign inv_bits = ~direct_lut_o;
+  assign sign_mod_o = sign_mod_temp; 
   always@(*)
     begin
     sign_mod_temp = inv_bits + 1;
     end 
       
-  // increment circuit 
-  wire [4:0] incr_o
-  // increment_circuit u_incr2 (.A(X[7:4]), .incr(X[3]), .S(incr_o[3:0]), .Co(incr_o[4]));
-  assign incr_o = inc_temp;
-  reg [4:0] inc_temp; 
+  // Increment Circuit 
+  wire [4:0] incr_o;
+  reg [4:0] incr_temp;
+  assign incr_o = incr_temp;
   always@(*)
     begin
-      inc_temp = X[7:4] + X[3];
-    end 
-      
+      incr_temp = X[7:4] + X[3];
+    end   
   
-  // 9 word OMS LUT
-  // same thing as Table IV 
+  // 9 Word OMS LUT, same as Table IV 
   reg [11:0] oms_lut_o;
   always@(*)
     begin
@@ -69,48 +64,52 @@ module lut_mult_8bit #(parameter A_const = 2) (input [7:0]X, output [15:0]C);
         6: oms_lut_o = 12'd13 * A_const;
         7: oms_lut_o = 12'd15 * A_const;
         8: oms_lut_o = 12'd2  * A_const;
-        default: oms_lut_o = 12'b0
+        default: oms_lut_o = 12'b0;
       endcase
     end 
         
-  // barrel shifter 
+  // Barrel Shifter 
   // add control bits, assume the same s0 and s1 equations as the paper
   // 12bit_barrel_shifter u_shift ( );
           
   wire s0; wire s1;
+  wire n00, n01, n02, n03;
+  wire n10; 
   // s0 
-  not u00 (n00, y[2]);
-  or  u01 (n01, y[1], n00);
+  not u00 (n00, incr_o[2]);
+  or  u01 (n01, incr_o[1], n00);
   not u02 (n02, n01);
-  or  u03 (n03, y[0], n02);
+  or  u03 (n03, incr_o[0], n02);
   not u04 (s0, n03);
   
   // s1
-  or s10  (n10, y[0], y[1]);
+  or s10  (n10, incr_o[0], incr_o[1]);
   not s11 (s1, n10);
   
   reg [15:0] temp_shift_out;
-  wire [15:0] shift_out assign = temp_shift_out;
+  wire [15:0] shift_out;
+  assign shift_out = temp_shift_out;
   always@(*)
     begin
     case({s1,s0})
-      2'b00: temp_shift_out = shift_in;
-      2'b01: temp_shift_out = shift_in << 1;
-      2'b10: temp_shift_out = shift_in << 2;
-      2'b11: temp_shift_out = shift_in << 3;
+      2'b00: temp_shift_out = oms_lut_o;
+      2'b01: temp_shift_out = oms_lut_o << 1;
+      2'b10: temp_shift_out = oms_lut_o << 2;
+      2'b11: temp_shift_out = oms_lut_o << 3;
     endcase 
     end
  
   
-  // add partial products to get final product
-        reg [15:0] C_temp; 
-   assign C = C_temp;
-   always@(*)
-      begin       
+   // add partial products to get final product
+  reg [15:0] C_temp; 
+  assign C = C_temp;
+  always@(*)
+    begin       
       C_temp = {4'b0,sign_mod_o} + {temp_shift_out[11:0],4'b0} ; // a[15:0], b[15:0]
-      end 
+    end 
   // C = {8'b0,sign_mod_o} + {temp_shift_out[15:0],4'b0} ; // a[15:0], b[15:0
   // 16bit_adder u_adder (.A(), .B(), .S(C);
+  
   
 endmodule 
  
